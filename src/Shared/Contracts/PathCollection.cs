ecca1b7bdabcffd9e3626ab8c2de82fdfdc588d6
Stefan Over <herdosratos@googlemail.com>
2019-06-14 20:10:47 +0200

    Added tests for path collection

--- src/Shared/Contracts/PathCollection.cs
+++ src/Shared/Contracts/PathCollection.cs
@@ -1,48 +1,70 @@
 ﻿namespace SSDTLifecycleExtension.Shared.Contracts
 {
+    using System;
+    using JetBrains.Annotations;
+
     public class PathCollection
     {
         /// <summary>
         /// Gets the absolute path of the publish profile (xml) used to generate the deployment script and report.
         /// </summary>
+        [NotNull]
         public string PublishProfilePath { get; }
 
         /// <summary>
         /// Gets the absolute directory of the new DACPAC.
         /// </summary>
+        [NotNull]
         public string NewDacpacDirectory { get; }
 
         /// <summary>
         /// Gets the absolute path of the new DACPAC.
         /// </summary>
+        [NotNull]
         public string NewDacpacPath { get; }
 
         /// <summary>
-        /// Gets the absolute path of the previous DACPAC.
+        /// Gets the absolute path of the previous DACPAC, if there is one.
         /// </summary>
+        [CanBeNull]
         public string PreviousDacpacPath { get; }
 
         /// <summary>
-        /// Gets the absolute path of where to create the deployment script.
+        /// Gets the absolute path of where to create the deployment script, if it should be created.
         /// </summary>
+        [CanBeNull]
         public string DeployScriptPath { get; }
 
         /// <summary>
-        /// Gets the absolute path of where to create the deployment report.
+        /// Gets the absolute path of where to create the deployment report, if it should be created.
         /// </summary>
+        [CanBeNull]
         public string DeployReportPath { get; }
 
-        public PathCollection(string publishProfilePath,
-                              string newDacpacDirectory,
-                              string newDacpacPath,
-                              string previousDacpacPath,
-                              string deployScriptPath,
-                              string deployReportPath)
+        /// <summary>
+        /// Initializes a new <see cref="PathCollection"/> instance for a given set of paths.
+        /// </summary>
+        /// <param name="publishProfilePath">The path of the publish profile to use during script or report creation.</param>
+        /// <param name="newDacpacDirectory">The directory, where the new DACPAC exists.</param>
+        /// <param name="newDacpacPath">The path of the new DACPAC.</param>
+        /// <param name="previousDacpacPath">The optional path of the previous DACPAC.</param>
+        /// <param name="deployScriptPath">The optional path of where to create the deploy script.</param>
+        /// <param name="deployReportPath">The optional path of where to create the deploy report.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="publishProfilePath"/>, <paramref name="newDacpacDirectory"/> or <paramref name="newDacpacPath"/> are <b>null</b>.</exception>
+        /// <exception cref="InvalidOperationException">Both <paramref name="deployScriptPath"/> and <paramref name="deployReportPath"/> are <b>null</b>.</exception>
+        public PathCollection([NotNull] string publishProfilePath,
+                              [NotNull] string newDacpacDirectory,
+                              [NotNull] string newDacpacPath,
+                              [CanBeNull] string previousDacpacPath,
+                              [CanBeNull] string deployScriptPath,
+                              [CanBeNull] string deployReportPath)
         {
-            PublishProfilePath = publishProfilePath;
-            NewDacpacDirectory = newDacpacDirectory;
-            NewDacpacPath = newDacpacPath;
+            PublishProfilePath = publishProfilePath ?? throw new ArgumentNullException(nameof(publishProfilePath));
+            NewDacpacDirectory = newDacpacDirectory ?? throw new ArgumentNullException(nameof(newDacpacDirectory));
+            NewDacpacPath = newDacpacPath ?? throw new ArgumentNullException(nameof(newDacpacPath));
             PreviousDacpacPath = previousDacpacPath;
+            if (deployScriptPath == null && deployReportPath == null)
+                throw new InvalidOperationException($"Either {nameof(deployScriptPath)}, {nameof(deployReportPath)}, or both must be provided.");
             DeployScriptPath = deployScriptPath;
             DeployReportPath = deployReportPath;
         }
---
+++ src/UnitTests/Shared/Contracts/PathCollectionTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+
+namespace SSDTLifecycleExtension.UnitTests.Shared.Contracts
+{
+    using System;
+    using SSDTLifecycleExtension.Shared.Contracts;
+
+    [TestFixture]
+    public class PathCollectionTests
+    {
+        [Test]
+        public void Constructor_ArgumentNullException_PublishProfilePath()
+        {
+            // Act & Assert
+            // ReSharper disable once ObjectCreationAsStatement
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => new PathCollection(null, null, null, null, null, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void Constructor_ArgumentNullException_NewDacpacDirectory()
+        {
+            // Arrange
+            const string publishProfilePath = "publishProfile";
+
+            // Act & Assert
+            // ReSharper disable once ObjectCreationAsStatement
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => new PathCollection(publishProfilePath, null, null, null, null, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void Constructor_ArgumentNullException_NewDacpacPath()
+        {
+            // Arrange
+            const string publishProfilePath = "publishProfile";
+            const string newDacpacDirectory = "_DIRECTORY_newDacpac";
+
+            // Act & Assert
+            // ReSharper disable once ObjectCreationAsStatement
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => new PathCollection(publishProfilePath, newDacpacDirectory, null, null, null, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void Constructor_InvalidOperationException_NeitherScriptPathNorDeployPathSet()
+        {
+            // Arrange
+            const string publishProfilePath = "publishProfile";
+            const string newDacpacDirectory = "_DIRECTORY_newDacpac";
+            const string newDacpacPath = "_PATH_newDacpac";
+
+            // Act & Assert
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<InvalidOperationException>(() => new PathCollection(publishProfilePath, newDacpacDirectory, newDacpacPath, null, null, null));
+        }
+
+        [Test]
+        [TestCase(true, true)]
+        [TestCase(true, false)]
+        [TestCase(false, true)]
+        public void Constructor_CorrectSettingOfProperties(bool setDeployScriptPath, bool setDeployReportPath)
+        {
+            // Arrange
+            const string publishProfilePath = "publishProfile";
+            const string newDacpacDirectory = "_DIRECTORY_newDacpac";
+            const string newDacpacPath = "_PATH_newDacpac";
+            const string previousDacpacPath = "_PATH_previousDacpac";
+            const string deployScriptPath = "_PATH_deployScript";
+            const string deployReportPath = "_PATH_deployReport";
+
+            // Act
+            var pc = new PathCollection(publishProfilePath,
+                                        newDacpacDirectory,
+                                        newDacpacPath,
+                                        previousDacpacPath,
+                                        setDeployScriptPath ? deployScriptPath : null,
+                                        setDeployReportPath ? deployReportPath : null);
+
+            // Assert
+            Assert.AreEqual(publishProfilePath, pc.PublishProfilePath);
+            Assert.AreEqual(newDacpacDirectory, pc.NewDacpacDirectory);
+            Assert.AreEqual(newDacpacPath, pc.NewDacpacPath);
+            Assert.AreEqual(previousDacpacPath, pc.PreviousDacpacPath);
+            if (setDeployScriptPath)
+                Assert.AreEqual(deployScriptPath, pc.DeployScriptPath);
+            else
+                Assert.IsNull(pc.DeployScriptPath);
+            if (setDeployReportPath)
+                Assert.AreEqual(deployReportPath, pc.DeployReportPath);
+            else
+                Assert.IsNull(pc.DeployReportPath);
+        }
+    }
+}
