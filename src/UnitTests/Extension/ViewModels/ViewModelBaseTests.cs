4640d4cb363c9be2d10fd94b47a62d00a695f058
Stefan Over <herdosratos@googlemail.com>
2019-06-18 18:35:32 +0200

    Implemented tests for ViewModelBase

---
+++ src/UnitTests/Extension/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+
+namespace SSDTLifecycleExtension.UnitTests.Extension.ViewModels
+{
+    using System.Collections.Generic;
+    using SSDTLifecycleExtension.ViewModels;
+
+    [TestFixture]
+    public class ViewModelBaseTests
+    {
+        [Test]
+        public void PropertyChanged_ImplicitName()
+        {
+            // Arrange
+            var changedProperties = new List<string>();
+            var vm = new ViewModelBaseTestImplementation();
+            vm.PropertyChanged += (sender,
+                                   args) =>
+            {
+                Assert.IsNotNull(sender);
+                Assert.AreSame(vm, sender);
+                Assert.IsNotNull(args);
+                changedProperties.Add(args.PropertyName);
+            };
+
+            // Act
+            vm.ImplicitName = "test";
+
+            // Assert
+            Assert.AreEqual("test", vm.ImplicitName);
+            Assert.IsNull(vm.ExplicitName);
+            Assert.AreEqual(1, changedProperties.Count);
+            Assert.AreEqual("ImplicitName", changedProperties[0]);
+        }
+
+        [Test]
+        public void PropertyChanged_ExplicitName()
+        {
+            // Arrange
+            var changedProperties = new List<string>();
+            var vm = new ViewModelBaseTestImplementation();
+            vm.PropertyChanged += (sender,
+                                   args) =>
+            {
+                Assert.IsNotNull(sender);
+                Assert.AreSame(vm, sender);
+                Assert.IsNotNull(args);
+                changedProperties.Add(args.PropertyName);
+            };
+
+            // Act
+            vm.ExplicitName = "test";
+
+            // Assert
+            Assert.IsNull(vm.ImplicitName);
+            Assert.AreEqual("test", vm.ExplicitName);
+            Assert.AreEqual(1, changedProperties.Count);
+            Assert.AreEqual("ExplicitNameExtra", changedProperties[0]);
+        }
+
+        private sealed class ViewModelBaseTestImplementation : ViewModelBase
+        {
+            private string _implicitName;
+            private string _explicitName;
+
+            internal string ImplicitName
+            {
+                get => _implicitName;
+                set
+                {
+                    if (value == _implicitName)
+                        return;
+                    _implicitName = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            internal string ExplicitName
+            {
+                get => _explicitName;
+                set
+                {
+                    if (value == _explicitName)
+                        return;
+                    _explicitName = value;
+                    OnPropertyChanged(nameof(ExplicitName) + "Extra");
+                }
+            }
+        }
+    }
+}
