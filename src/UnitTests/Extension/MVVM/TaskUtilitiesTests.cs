59cbc52280afcb3f92c1604cd9acf1e16bab971e
Stefan Over <herdosratos@googlemail.com>
2019-06-19 19:56:04 +0200

    Implemented tests for TaskUtilities

---
+++ src/UnitTests/Extension/MVVM/TaskUtilitiesTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+namespace SSDTLifecycleExtension.UnitTests.Extension.MVVM
+{
+    using System;
+    using System.Threading.Tasks;
+    using Moq;
+    using SSDTLifecycleExtension.MVVM;
+
+    [TestFixture]
+    public class TaskUtilitiesTests
+    {
+        [Test]
+        public void FireAndForget_ArgumentNullException_Task()
+        {
+            // Act & Assert
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => (null as Task).FireAndForget(null, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void FireAndForget_ArgumentNullException_Command()
+        {
+            // Arrange
+            var task = Task.CompletedTask;
+
+            // Act & Assert
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => task.FireAndForget(null, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void FireAndForget_ArgumentNullException_Handler()
+        {
+            // Arrange
+            var task = Task.CompletedTask;
+            var commandMock = Mock.Of<IAsyncCommand>();
+
+            // Act & Assert
+            // ReSharper disable AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => task.FireAndForget(commandMock, null));
+            // ReSharper restore AssignNullToNotNullAttribute
+        }
+
+        [Test]
+        public void FireAndForget_RunCompletely()
+        {
+            // Arrange
+            var hasRun = false;
+            var task = Task.Run(() => hasRun = true);
+            var commandMock = Mock.Of<IAsyncCommand>();
+            var errorHandlerMock = Mock.Of<IErrorHandler>();
+
+            // Act
+            task.FireAndForget(commandMock, errorHandlerMock);
+
+            // Assert
+            Assert.IsTrue(hasRun);
+        }
+
+        [Test]
+        public void FireAndForget_CallErrorHandlerOnException()
+        {
+            // Arrange
+            var task = Task.Run(() => throw new InvalidOperationException("test exception"));
+            var commandMock = Mock.Of<IAsyncCommand>();
+            var errorHandlerMock = new Mock<IErrorHandler>();
+
+            // Act
+            task.FireAndForget(commandMock, errorHandlerMock.Object);
+
+            // Assert
+            errorHandlerMock.Verify(m => m.HandleError(commandMock, It.IsNotNull<InvalidOperationException>()), Times.Once);
+        }
+    }
+}
