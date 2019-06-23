532cb7809cf0c7f2994f4f92656d56174f11e408
Stefan Over <herdosratos@googlemail.com>
2019-06-23 17:03:43 +0200

    Extended tests on DefaultConstraint

--- src/UnitTests/Shared/Contracts/DefaultConstraintTests.cs
+++ src/UnitTests/Shared/Contracts/DefaultConstraintTests.cs
@@ -3,6 +3,7 @@
 namespace SSDTLifecycleExtension.UnitTests.Shared.Contracts
 {
     using System;
+    using Microsoft.VisualStudio.Settings;
     using SSDTLifecycleExtension.Shared.Contracts;
 
     [TestFixture]
@@ -63,6 +64,173 @@ namespace SSDTLifecycleExtension.UnitTests.Shared.Contracts
             Assert.AreEqual(tableName, dc.TableName);
             Assert.AreEqual(columnName, dc.ColumnName);
             Assert.AreEqual(constraintName, dc.ConstraintName);
+        }
+
+        [Test]
+        public void Equals_Object_Null_False()
+        {
+            // Arrange
+            var instance = new DefaultConstraint("a", "b", "c", "d");
+
+            // Act
+            var equals = instance.Equals(null as object);
+
+            // Assert
+            Assert.IsFalse(equals);
+        }
+
+        [Test]
+        public void Equals_Object_SameInstance_True()
+        {
+            // Arrange
+            var instance = new DefaultConstraint("a", "b", "c", "d");
+
+            // Act
+            var equals = instance.Equals((object) instance);
+
+            // Assert
+            Assert.IsTrue(equals);
+        }
+
+        [Test]
+        public void Equals_Object_DifferentType_False()
+        {
+            // Arrange
+            var instance = new DefaultConstraint("a", "b", "c", "d");
+
+            // Act
+            var equals = instance.Equals(new Version(1, 0));
+
+            // Assert
+            Assert.IsFalse(equals);
+        }
+
+        [Test]
+        [TestCase("a", "b", "c", "d", true)]
+        [TestCase("a", "b", "c", null, false)]
+        [TestCase("a1", "b", "c", "d", false)]
+        [TestCase("a", "b1", "c", "d", false)]
+        [TestCase("a", "b", "c1", "d", false)]
+        [TestCase("a", "b", "c", "d1", false)]
+        public void Equals_Object_TwoInstances(string tableSchema, string tableName, string columnName, string constraintName, bool expectedEquals)
+        {
+            // Arrange
+            var instance1 = new DefaultConstraint("a", "b", "c", "d");
+            var instance2 = new DefaultConstraint(tableSchema, tableName, columnName, constraintName);
+
+            // Act
+            var equals = instance1.Equals((object)instance2);
+
+            // Assert
+            Assert.AreEqual(expectedEquals, equals);
+        }
+
+        [Test]
+        public void Equals_Typed_Null_False()
+        {
+            // Arrange
+            var instance = new DefaultConstraint("a", "b", "c", "d");
+
+            // Act
+            var equals = instance.Equals(null);
+
+            // Assert
+            Assert.IsFalse(equals);
+        }
+
+        [Test]
+        public void Equals_Typed_SameInstance_True()
+        {
+            // Arrange
+            var instance = new DefaultConstraint("a", "b", "c", "d");
+
+            // Act
+            var equals = instance.Equals(instance);
+
+            // Assert
+            Assert.IsTrue(equals);
+        }
+
+        [Test]
+        [TestCase("a", "b", "c", "d", true)]
+        [TestCase("a", "b", "c", null, false)]
+        [TestCase("a1", "b", "c", "d", false)]
+        [TestCase("a", "b1", "c", "d", false)]
+        [TestCase("a", "b", "c1", "d", false)]
+        [TestCase("a", "b", "c", "d1", false)]
+        public void Equals_Typed_TwoInstances(string tableSchema, string tableName, string columnName, string constraintName, bool expectedEquals)
+        {
+            // Arrange
+            var instance1 = new DefaultConstraint("a", "b", "c", "d");
+            var instance2 = new DefaultConstraint(tableSchema, tableName, columnName, constraintName);
+
+            // Act
+            var equals = instance1.Equals(instance2);
+
+            // Assert
+            Assert.AreEqual(expectedEquals, equals);
+        }
+
+        [Test]
+        [TestCase("a", "b", "c", "d", true)]
+        [TestCase("a", "b", "c", null, false)]
+        [TestCase("a1", "b", "c", "d", false)]
+        [TestCase("a", "b1", "c", "d", false)]
+        [TestCase("a", "b", "c1", "d", false)]
+        [TestCase("a", "b", "c", "d1", false)]
+        public void GetHashCode_HashCodeComparison(string tableSchema, string tableName, string columnName, string constraintName, bool expectedSameHashCode)
+        {
+            // Arrange
+            var instance1 = new DefaultConstraint("a", "b", "c", "d");
+            var instance2 = new DefaultConstraint(tableSchema, tableName, columnName, constraintName);
+
+            // Act
+            var i1HashCode = instance1.GetHashCode();
+            var i2HashCode = instance2.GetHashCode();
+            var sameHashCode = i1HashCode == i2HashCode;
+
+            // Assert
+            Assert.AreEqual(expectedSameHashCode, sameHashCode);
+        }
+
+        [Test]
+        [TestCase("a", "b", "c", "d", true)]
+        [TestCase("a", "b", "c", null, false)]
+        [TestCase("a1", "b", "c", "d", false)]
+        [TestCase("a", "b1", "c", "d", false)]
+        [TestCase("a", "b", "c1", "d", false)]
+        [TestCase("a", "b", "c", "d1", false)]
+        public void EqualityOperator(string tableSchema, string tableName, string columnName, string constraintName, bool expectedEquality)
+        {
+            // Arrange
+            var instance1 = new DefaultConstraint("a", "b", "c", "d");
+            var instance2 = new DefaultConstraint(tableSchema, tableName, columnName, constraintName);
+
+            // Act
+            var equality = instance1 == instance2;
+
+            // Assert
+            Assert.AreEqual(expectedEquality, equality);
+        }
+
+        [Test]
+        [TestCase("a", "b", "c", "d", false)]
+        [TestCase("a", "b", "c", null, true)]
+        [TestCase("a1", "b", "c", "d", true)]
+        [TestCase("a", "b1", "c", "d", true)]
+        [TestCase("a", "b", "c1", "d", true)]
+        [TestCase("a", "b", "c", "d1", true)]
+        public void InequalityOperator(string tableSchema, string tableName, string columnName, string constraintName, bool expectedInequality)
+        {
+            // Arrange
+            var instance1 = new DefaultConstraint("a", "b", "c", "d");
+            var instance2 = new DefaultConstraint(tableSchema, tableName, columnName, constraintName);
+
+            // Act
+            var inequality = instance1 != instance2;
+
+            // Assert
+            Assert.AreEqual(expectedInequality, inequality);
         }
     }
 }
