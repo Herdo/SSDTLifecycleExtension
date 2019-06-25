0e27c1fbcba953b1396a34d53addbf11cc424879
Stefan Over <herdosratos@googlemail.com>
2019-06-25 17:26:44 +0200

    Implemented tests for BaseStateModel

---
+++ src/UnitTests/Shared/Models/BaseStateModelTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+
+namespace SSDTLifecycleExtension.UnitTests.Shared.Models
+{
+    using System;
+    using System.Threading.Tasks;
+    using JetBrains.Annotations;
+    using SSDTLifecycleExtension.Shared.Contracts.Enums;
+    using SSDTLifecycleExtension.Shared.Models;
+
+    [TestFixture]
+    public class BaseStateModelTests
+    {
+        [Test]
+        public void Constructor_ArgumentNullException_HandleWorkInProgressChanged()
+        {
+            // Act & Assert
+            // ReSharper disable once ObjectCreationAsStatement
+            // ReSharper disable once AssignNullToNotNullAttribute
+            Assert.Throws<ArgumentNullException>(() => new BaseStateModelTestImplementation(null));
+        }
+
+        [Test]
+        public void Constructor_CorrectInitialization()
+        {
+            // Arrange
+            // ReSharper disable once ConvertToLocalFunction
+            Func<bool, Task> handlerFunc = b => Task.CompletedTask;
+
+            // Act
+            var model = new BaseStateModelTestImplementation(handlerFunc);
+
+            // Assert
+            Assert.AreSame(handlerFunc, model.HandleWorkInProgressChanged);
+            Assert.AreEqual(StateModelState.Initialized, model.CurrentState);
+            Assert.IsNull(model.Result);
+        }
+
+        [Test]
+        public void CurrentState_Get_Set()
+        {
+            // Arrange
+            Task HandlerFunc(bool b) => Task.CompletedTask;
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var model = new BaseStateModelTestImplementation(HandlerFunc);
+
+            // Act
+            model.CurrentState = StateModelState.PathsVerified;
+
+            // Assert
+            Assert.AreEqual(StateModelState.PathsVerified, model.CurrentState);
+        }
+
+        [Test]
+        public void Result_Get_Set()
+        {
+            // Arrange
+            Task HandlerFunc(bool b) => Task.CompletedTask;
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var model = new BaseStateModelTestImplementation(HandlerFunc);
+
+            // Act
+            model.Result = true;
+
+            // Assert
+            Assert.AreEqual(true, model.Result);
+        }
+
+        private class BaseStateModelTestImplementation : BaseStateModel
+        {
+            internal BaseStateModelTestImplementation([NotNull] Func<bool, Task> handleWorkInProgressChanged)
+                : base(handleWorkInProgressChanged)
+            {
+            }
+        }
+    }
+}
