822d6abf5bbc15ad57babde912cc28b091e06330
Stefan Over <herdosratos@googlemail.com>
2019-07-01 19:41:01 +0200

    Added test to make sure that ScriptModifier.RemoveSqlCmdStatements is always the highest value

---
+++ src/UnitTests/Shared/Enums/ScriptModifierTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace SSDTLifecycleExtension.UnitTests.Shared.Enums
+{
+    using SSDTLifecycleExtension.Shared.Contracts.Enums;
+
+    [TestFixture]
+    public class ScriptModifierTest
+    {
+        [Test]
+        public void RemoveSqlCmdStatements_AlwaysHaveHighestValue_ForSortOrder()
+        {
+            // Act
+            var intValue = (int)ScriptModifier.RemoveSqlCmdStatements;
+
+            // Assert
+            Assert.AreEqual(int.MaxValue, intValue);
+        }
+    }
+}
